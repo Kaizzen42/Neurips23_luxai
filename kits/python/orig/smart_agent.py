aba91461e9b1ed70864585fda0c777cd95beb1da
kaizzen <kaizzen42@gmail.com>
2023-10-07 16:33:57 -0700

    Added separate folders for self competetion. Factories next to ice. Light robots being built.

--- kits/python/orig/smart_agent.py
+++ kits/python/orig/smart_agent.py
@@ -1,9 +1,8 @@
-from lux.kit import obs_to_game_state, GameState
-from lux.config import EnvConfig
+from lux.kit import obs_to_game_state, GameState, EnvConfig
 from lux.utils import direction_to, my_turn_to_place_factory
 import numpy as np
 import sys
-class Agent():
+class SmartAgent():
     def __init__(self, player: str, env_cfg: EnvConfig) -> None:
         self.player = player
         self.opp_player = "player_1" if self.player == "player_0" else "player_0"
@@ -29,20 +28,27 @@ class Agent():
             if factories_to_place > 0 and my_turn_to_place:
                 # we will spawn our factory in a random location with 150 metal and water if it is our turn to place
                 potential_spawns = np.array(list(zip(*np.where(obs["board"]["valid_spawns_mask"] == 1))))
-                spawn_loc = potential_spawns[np.random.randint(0, len(potential_spawns))]
+
+                # First modification: lets spawn near ice.
+                ice_map = game_state.board.ice
+                ice_tile_locations = np.argwhere(ice_map == 1)
+                import math
+                min_ice_dist = float(math.inf)
+                best_spawn_choice = potential_spawns[np.random.randint(0, len(potential_spawns))]
+                for spawn_choice in potential_spawns:
+                    ice_tile_distances = np.mean((ice_tile_locations - spawn_choice) ** 2, 1)
+                    # closest_ice_tile = ice_tile_locations[np.argmin(ice_tile_distances)]
+                    closest_ice_tile_dist = min(ice_tile_distances)
+                    if closest_ice_tile_dist <= min_ice_dist:
+                        best_spawn_choice = spawn_choice
+                        min_ice_dist = closest_ice_tile_dist
+                # spawn_loc = potential_spawns[np.random.randint(0, len(potential_spawns))]
+                spawn_loc = best_spawn_choice
                 return dict(spawn=spawn_loc, metal=150, water=150)
             return dict()
 
     def act(self, step: int, obs, remainingOverageTime: int = 60):
         actions = dict()
-        
-        """
-        optionally do forward simulation to simulate positions of units, lichen, etc. in the future
-        from lux.forward_sim import forward_sim
-        forward_obs = forward_sim(obs, self.env_cfg, n=2)
-        forward_game_states = [obs_to_game_state(step + i, self.env_cfg, f_obs) for i, f_obs in enumerate(forward_obs)]
-        """
-
         game_state = obs_to_game_state(step, self.env_cfg, obs)
         factories = game_state.factories[self.player]
         game_state.teams[self.player].place_first
@@ -51,8 +57,9 @@ class Agent():
             if factory.power >= self.env_cfg.ROBOTS["HEAVY"].POWER_COST and \
             factory.cargo.metal >= self.env_cfg.ROBOTS["HEAVY"].METAL_COST:
                 actions[unit_id] = factory.build_heavy()
-            if factory.water_cost(game_state) <= factory.cargo.water / 5 - 200:
-                actions[unit_id] = factory.water()
+            if self.env_cfg.max_episode_length - game_state.real_env_steps < 50:
+                if factory.water_cost(game_state) <= factory.cargo.water:
+                    actions[unit_id] = factory.water()
             factory_tiles += [factory.pos]
             factory_units += [factory]
         factory_tiles = np.array(factory_tiles)
@@ -88,7 +95,7 @@ class Agent():
                     direction = direction_to(unit.pos, closest_factory_tile)
                     if adjacent_to_factory:
                         if unit.power >= unit.action_queue_cost(game_state):
-                            actions[unit_id] = [unit.transfer(direction, 0, unit.cargo.ice, repeat=0)]
+                            actions[unit_id] = [unit.transfer(direction, 0, unit.cargo.ice, repeat=0, n=1)]
                     else:
                         move_cost = unit.move_cost(game_state, direction)
                         if move_cost is not None and unit.power >= move_cost + unit.action_queue_cost(game_state):
